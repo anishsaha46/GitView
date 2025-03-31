939050d515254c1144f59e04b8137dd5f90c9bbe
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-03-31 23:53:31 +0530

    Add File Structure Tree component with TypeScript definitions for file and directory nodes

---
+++ gitview/components/file-structure-tree.tsx
@@ -0,0 +1,9 @@
+"use client"
+import { useState } from "react"
+import  { ChevronDown, ChevronRight, File, Folder } from "lucide-react"
+
+interface FileNode{
+    path:string
+    type: "file" | "dir"
+    children?: FileNode[]
+}
