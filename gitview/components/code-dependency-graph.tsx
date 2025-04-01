74cb46413f337214713f49cad46192b31c3706cb
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-04-01 23:19:03 +0530

    Enhance code dependency graph simulation with force layout configuration

--- gitview/components/code-dependency-graph.tsx
+++ gitview/components/code-dependency-graph.tsx
@@ -31,5 +31,20 @@ interface DependencyNode {
 
         // clear previous graph elements
         d3.select(svgRef.current).selectAll("*").remove()
+
+    // Create the simulation with forces
+    const simulation = d3
+      .forceSimulation()
+      .nodes(data.nodes as d3.SimulationNodeDatum[])
+      .force(
+        "link",
+        d3
+          .forceLink()
+          .id((d: any) => d.id)
+          .links(data.links)
+          .distance(100),
+      )
+      .force("charge", d3.forceManyBody().strength(-300))
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .force("collision", d3.forceCollide().radius(50))
     })
-    }
