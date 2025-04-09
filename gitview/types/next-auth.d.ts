5238a40bf9f5b6e6967e2a5f78840a4ae565b579
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-04-09 11:13:38 +0530

    fix: extend Session interface in next-auth types to include DefaultSession properties

--- gitview/types/next-auth.d.ts
+++ gitview/types/next-auth.d.ts
@@ -1,7 +1,8 @@
 import "next-auth"
+import { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
-  interface Session {
+  interface Session extends DefaultSession {
     accessToken?: string
     user?: {
       name?: string | null
